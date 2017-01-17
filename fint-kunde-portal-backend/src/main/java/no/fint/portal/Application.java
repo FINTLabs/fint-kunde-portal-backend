9e5a70954424180fa8dab3d8df9dfa5e4ab2e8b8
Oystein Amundsen <oystein.amundsen@bouvet.no>
2017-01-17 09:45:24 +0100

    Fixed forwarding to index.html on requests to missing controller endpoints

--- fint-kunde-portal-backend/src/main/java/no/fint/portal/Application.java
+++ fint-kunde-portal-backend/src/main/java/no/fint/portal/Application.java
@@ -5,14 +5,35 @@ import no.rogfk.hateoas.extension.annotations.EnableHalHypermediaSupport;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+
+import java.util.Properties;
 
 @EnableHalHypermediaSupport
 @EnableSpringfox
 @SpringBootApplication
 @EnableAutoConfiguration
-public class Application {
-
+public class Application  {
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+    SpringApplication app = new SpringApplication(Application.class);
+
+    // Modify default properties
+    Properties        properties = new Properties();
+    properties.put("spring.mvc.throw-exception-if-no-handler-found", true);
+    app.setDefaultProperties(properties);
+
+    // Run spring application
+    app.run(args);
+  }
+
+  @Bean
+  public EmbeddedServletContainerCustomizer containerCustomizer() {
+    return (container -> {
+      ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/");
+      container.addErrorPages(error404Page);
+    });
   }
 }
--- fint-kunde-portal-backend/src/main/java/no/fint/portal/customer/controller/GlobalExceptionHandler.java
+++ fint-kunde-portal-backend/src/main/java/no/fint/portal/customer/controller/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package no.fint.portal.customer.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.NoHandlerFoundException;
@@ -7,11 +9,13 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 /**
  * Maps every request spring cannot handle to deliver index.html
  */
-@SuppressWarnings("ALL")
 @ControllerAdvice
-class AngularController {
+class GlobalExceptionHandler {
+  private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
   @ExceptionHandler(NoHandlerFoundException.class)
   public String handle(Exception ex) {
+    logger.error("Exception caught: NoHandlerFoundException - " + ex.getMessage());
     return "forward:/index.html";
   }
 }
