29f808904821775a3a0a043b5e0de7cf935668f5
Asgeir Nilsen <asgeir@fintprosjektet.no>
2017-12-19 13:03:11 +0100

    wip with some testing with embedded LDAP.
    TODO: Move embedded LDAP to test classpath.

--- src/test/java/no/fint/portal/customer/test/TestApplication.java
+++ src/test/java/no/fint/portal/customer/test/TestApplication.java
@@ -4,16 +4,16 @@ import com.github.springfox.loader.EnableSpringfox;
 import no.rogfk.hateoas.extension.annotations.EnableHalHypermediaSupport;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.test.context.ActiveProfiles;
 
 @EnableHalHypermediaSupport
 @EnableSpringfox
 @SpringBootApplication
-@ActiveProfiles("test")
 public class TestApplication {
   public static void main(String[] args) {
-    SpringApplication app = new SpringApplication(no.fint.portal.Application.class);
+    SpringApplication app = new SpringApplication(TestApplication.class);
 
+    // TODO: Doesn't work yet -- Spring problems.
+    app.setAdditionalProfiles("test");
     // Run spring application
     app.run(args);
   }
