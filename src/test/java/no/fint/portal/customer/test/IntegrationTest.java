b72b118d0d4250181169b69b6d96c7b5d5b65687
Frode Sjovatsen <frode@fintprosjektet.no>
2018-08-05 16:20:35 +0200

    updated test and test ldap schema to handle primary assets

--- src/test/java/no/fint/portal/customer/test/IntegrationTest.java
+++ src/test/java/no/fint/portal/customer/test/IntegrationTest.java
@@ -31,16 +31,16 @@ public class IntegrationTest {
   @Test
   public void assets() throws Exception {
     mockMvc.perform(get("/api/assets/testing/")).andExpect(status().isOk());
-    mockMvc.perform(post("/api/assets/testing/").content("{ \"assetId\": \"test.no\", \"description\": \"Test Norge AS\" }").contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(201));
-    mockMvc.perform(get("/api/assets/testing/test_no")).andExpect(status().isOk()).andExpect(jsonPath("$.name").value(equalTo("test_no")));
-    mockMvc.perform(put("/api/assets/testing/test_no").content("{ \"assetId\": \"test.no\", \"name\": \"test_no\", \"description\": \"Test Mer Norge AS\" }").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(jsonPath("$.description").value(equalTo("Test Mer Norge AS")));
+    mockMvc.perform(post("/api/assets/testing/").content("{ \"assetId\": \"test\", \"description\": \"Test Norge AS\" }").contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(201));
+    mockMvc.perform(get("/api/assets/testing/test_test_no")).andExpect(status().isOk()).andExpect(jsonPath("$.name").value(equalTo("test_test_no")));
+    mockMvc.perform(put("/api/assets/testing/test_test_no").content("{ \"assetId\": \"test.no\", \"name\": \"test_test_no\", \"description\": \"Test Mer Norge AS\" }").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(jsonPath("$.description").value(equalTo("Test Mer Norge AS")));
     mockMvc.perform(get("/api/adapters/testing")).andExpect(status().isOk());
     mockMvc.perform(post("/api/adapters/testing").content("{ \"name\": \"testAdapter\", \"note\": \"Test Adapter\", \"secret\": \"Open Sesame!\", \"shortDescription\": \"This is a Test Adapter\" }").contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(201));
     mockMvc.perform(get("/api/adapters/testing/testadapter")).andExpect(status().isOk()).andExpect(jsonPath("$.name").value(equalTo("testAdapter")));
     mockMvc.perform(get("/api/adapters/testing/testadapter/secret")).andExpect(status().isOk()).andExpect(content().string(containsString("_ClientSecret")));
     mockMvc.perform(put("/api/adapters/testing/testadapter/password").content("This is the new password").contentType(MediaType.TEXT_PLAIN)).andExpect(status().isOk());
     mockMvc.perform(put("/api/adapters/testing/testadapter").content("{ \"name\": \"testadapter\", \"note\": \"Test Adapter With New Note\", \"shortDescription\": \"This is a Brand Spanking New Test Adapter\" }").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(jsonPath("$.shortDescription").value(containsString("Spanking")));
-    mockMvc.perform(put("/api/assets/testing/test_no/adapters/testadapter")).andExpect(status().is(204));
+    mockMvc.perform(put("/api/assets/testing/test_test_no/adapters/testadapter")).andExpect(status().is(204));
     mockMvc.perform(put("/api/components/administrasjon_personal/testing/adapters/testadapter").contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(204));
     mockMvc.perform(delete("/api/components/administrasjon_personal/testing/adapters/testadapter")).andExpect(status().is(204));
     mockMvc.perform(delete("/api/components/administrasjon_personal/testing/adapters/testadapter")).andExpect(status().is(204));
@@ -51,12 +51,12 @@ public class IntegrationTest {
     mockMvc.perform(get("/api/clients/testing/testclient/secret")).andExpect(status().isOk()).andExpect(content().string(containsString("_ClientSecret")));
     mockMvc.perform(put("/api/clients/testing/testclient").content("{ \"name\": \"testclient\", \"note\": \"Testing Client\", \"shortDescription\": \"This is an updated Test Client.\" }").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(jsonPath("$.shortDescription").value(containsString("updated")));
     mockMvc.perform(put("/api/clients/testing/testclient/password").content("This is the new password").contentType(MediaType.TEXT_PLAIN)).andExpect(status().isOk());
-    mockMvc.perform(put("/api/assets/testing/test_no/clients/testclient")).andExpect(status().is(204));
+    mockMvc.perform(put("/api/assets/testing/test_test_no/clients/testclient")).andExpect(status().is(204));
     mockMvc.perform(put("/api/components/administrasjon_personal/testing/clients/testclient").contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(204));
-    mockMvc.perform(delete("/api/assets/testing/test_no/clients/testclient")).andExpect(status().is(204));
+    mockMvc.perform(delete("/api/assets/testing/test_test_no/clients/testclient")).andExpect(status().is(204));
     mockMvc.perform(delete("/api/components/administrasjon_personal/testing/clients/testclient")).andExpect(status().is(204));
     mockMvc.perform(delete("/api/clients/testing/testclient")).andExpect(status().is(204));
-    mockMvc.perform(delete("/api/assets/testing/test_no")).andExpect(status().is(204));
+    mockMvc.perform(delete("/api/assets/testing/test_test_no")).andExpect(status().is(204));
   }
 
   @Test
