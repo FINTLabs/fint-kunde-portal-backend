882402a4e5e41cff8abec9abd284aa524e2085ec
Asgeir Nilsen <asgeir@fintprosjektet.no>
2018-10-18 15:11:50 +0200

    Authorize requests instead of ignoring security.
    This has the added side effect that Spring Security's default cache
    control headers are added, solving a compatibility issue with
    Internet Explorer.
    
    Also remove security.basic.enabled - this property has no effect in
    Spring Boot 2.

--- src/main/java/no/fint/portal/ApplicationSecurity.java
+++ src/main/java/no/fint/portal/ApplicationSecurity.java
@@ -1,16 +1,17 @@
 package no.fint.portal;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
-    public void configure(WebSecurity web) throws Exception {
-        web
-           .ignoring()
-               .antMatchers("/**");
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+          .authorizeRequests()
+          .antMatchers("/**")
+          .permitAll();
     }
 }
