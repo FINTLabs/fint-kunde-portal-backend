942bbd88c6a9aad6dfd0de7e75374dd6db877c34
Asgeir Nilsen <asgeir@fintprosjektet.no>
2018-10-19 09:53:09 +0200

    Clarified mockMvc tests using URL templates and variables.

--- src/test/java/no/fint/portal/customer/test/IntegrationTest.java
+++ src/test/java/no/fint/portal/customer/test/IntegrationTest.java
@@ -19,67 +19,76 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class IntegrationTest {
 
-  @Autowired
-  MockMvc mockMvc;
+    @Autowired
+    MockMvc mockMvc;
 
-  @Test
-  public void components() throws Exception {
-    mockMvc.perform(get("/api/components")).andExpect(status().isOk());
-    mockMvc.perform(get("/api/components/administrasjon_personal")).andExpect(status().isOk()).andExpect(jsonPath("$.name").value(equalTo("administrasjon_personal")));
-  }
+    String org = "testing";
+    String component = "administrasjon_personal";
+    String asset = "test_test_no";
+    String adapter = "testadapter@adapter.test.no";
+    String client = "testclient@client.test.no";
+    String contact1 = "12345678901";
+    String contact2 = "23456789012";
 
-  @Test
-  public void assets() throws Exception {
-    mockMvc.perform(get("/api/assets/testing/")).andExpect(status().isOk());
-    mockMvc.perform(post("/api/assets/testing/").content("{ \"assetId\": \"test\", \"description\": \"Test Norge AS\" }").contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(201));
-    mockMvc.perform(get("/api/assets/testing/test_test_no")).andExpect(status().isOk()).andExpect(jsonPath("$.name").value(equalTo("test_test_no")));
-    mockMvc.perform(put("/api/assets/testing/test_test_no").content("{ \"assetId\": \"test.no\", \"name\": \"test_test_no\", \"description\": \"Test Mer Norge AS\" }").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(jsonPath("$.description").value(equalTo("Test Mer Norge AS")));
-    mockMvc.perform(get("/api/adapters/testing")).andExpect(status().isOk());
-    mockMvc.perform(post("/api/adapters/testing").content("{ \"name\": \"testAdapter\", \"note\": \"Test Adapter\", \"secret\": \"Open Sesame!\", \"shortDescription\": \"This is a Test Adapter\" }").contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(201));
-    mockMvc.perform(get("/api/adapters/testing/testadapter@adapter.test.no")).andExpect(status().isOk()).andExpect(jsonPath("$.name").value(equalTo("testAdapter@adapter.test.no")));
-    mockMvc.perform(get("/api/adapters/testing/testadapter@adapter.test.no/secret")).andExpect(status().isOk()).andExpect(content().string(containsString("_ClientSecret")));
-    mockMvc.perform(put("/api/adapters/testing/testadapter@adapter.test.no/password").content("This is the new password").contentType(MediaType.TEXT_PLAIN)).andExpect(status().isOk());
-    mockMvc.perform(put("/api/adapters/testing/testadapter@adapter.test.no").content("{ \"name\": \"testadapter@adapter.test.no\", \"note\": \"Test Adapter With New Note\", \"shortDescription\": \"This is a Brand Spanking New Test Adapter\" }").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(jsonPath("$.shortDescription").value(containsString("Spanking")));
-    mockMvc.perform(put("/api/assets/testing/test_test_no/adapters/testadapter@adapter.test.no")).andExpect(status().is(204));
-    mockMvc.perform(put("/api/components/administrasjon_personal/testing/adapters/testadapter@adapter.test.no").contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(204));
-    mockMvc.perform(delete("/api/components/administrasjon_personal/testing/adapters/testadapter@adapter.test.no")).andExpect(status().is(204));
-    mockMvc.perform(delete("/api/components/administrasjon_personal/testing/adapters/testadapter@adapter.test.no")).andExpect(status().is(204));
-    mockMvc.perform(delete("/api/adapters/testing/testadapter@adapter.test.no")).andExpect(status().is(204));
-    mockMvc.perform(get("/api/clients/testing")).andExpect(status().is(200));
-    mockMvc.perform(post("/api/clients/testing").content("{ \"name\": \"testclient\", \"note\": \"Test Client\", \"secret\": \"password\", \"shortDescription\": \"This is a Test Client.\" }").contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(201)).andExpect(jsonPath("$.name").value(equalTo("testclient@client.test.no")));
-    mockMvc.perform(get("/api/clients/testing/testclient@client.test.no")).andExpect(status().isOk()).andExpect(jsonPath("$.name").value(equalTo("testclient@client.test.no")));
-    mockMvc.perform(get("/api/clients/testing/testclient@client.test.no/secret")).andExpect(status().isOk()).andExpect(content().string(containsString("_ClientSecret")));
-    mockMvc.perform(put("/api/clients/testing/testclient@client.test.no").content("{ \"name\": \"testclient@client.test.no\", \"note\": \"Testing Client\", \"shortDescription\": \"This is an updated Test Client.\" }").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(jsonPath("$.shortDescription").value(containsString("updated")));
-    mockMvc.perform(put("/api/clients/testing/testclient@client.test.no/password").content("This is the new password").contentType(MediaType.TEXT_PLAIN)).andExpect(status().isOk());
-    mockMvc.perform(put("/api/assets/testing/test_test_no/clients/testclient@client.test.no")).andExpect(status().is(204));
-    mockMvc.perform(put("/api/components/administrasjon_personal/testing/clients/testclient@client.test.no").contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(204));
-    mockMvc.perform(delete("/api/assets/testing/test_test_no/clients/testclient@client.test.no")).andExpect(status().is(204));
-    mockMvc.perform(delete("/api/components/administrasjon_personal/testing/clients/testclient@client.test.no")).andExpect(status().is(204));
-    mockMvc.perform(delete("/api/clients/testing/testclient@client.test.no")).andExpect(status().is(204));
-    mockMvc.perform(delete("/api/assets/testing/test_test_no")).andExpect(status().is(204));
-  }
+    @Test
+    public void components() throws Exception {
+        mockMvc.perform(get("/api/components")).andExpect(status().isOk());
+        mockMvc.perform(get("/api/components/{component}", component)).andExpect(status().isOk()).andExpect(jsonPath("$.name").value(equalTo(component)));
+    }
 
-  @Test
-  public void contacts() throws Exception {
-    mockMvc.perform(get("/api/contacts")).andExpect(status().is(200));
-    mockMvc.perform(get("/api/contacts/12345678901")).andExpect(status().is(200)).andExpect(jsonPath("$.nin").value(containsString("12345678901")));
-    mockMvc.perform(put("/api/contacts/12345678901").content("{ \"nin\": \"12345678901\", \"firstName\": \"Tore Martin\", \"lastName\": \"Testesen\", \"mail\": \"test123@example.com\", \"mobile\": \"98765431\" }").contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(200)).andExpect(jsonPath("$.mail").value(equalTo("test123@example.com")));
-    mockMvc.perform(post("/api/contacts").content("{ \"firstName\": \"John\", \"lastName\": \"Doe\", \"mail\": \"john.doe@example.com\", \"mobile\": \"1-800-555-1212\", \"nin\": \"00000000000\" }").contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(201)).andExpect(jsonPath("$.nin").value(equalTo("00000000000")));
-    mockMvc.perform(delete("/api/contacts/00000000000")).andExpect(status().is(204));
-  }
+    @Test
+    public void assets() throws Exception {
 
-  @Test
-  public void organisation() throws Exception {
-    mockMvc.perform(get("/api/organisations/testing/")).andExpect(status().is(200));
-    mockMvc.perform(put("/api/organisations/testing/").content("{ \"displayName\": \"Testing Unlimited\" }").contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(200)).andExpect(jsonPath("$.displayName").value(containsString("Unlimited")));
-    mockMvc.perform(put("/api/organisations/testing/components/administrasjon_personal")).andExpect(status().is(204));
-    mockMvc.perform(delete("/api/organisations/testing/components/administrasjon_personal")).andExpect(status().is(204));
-    mockMvc.perform(put("/api/organisations/testing/contacts/legal/12345678901")).andExpect(status().is(204));
-    mockMvc.perform(get("/api/organisations/testing/contacts/legal")).andExpect(status().is(200)).andExpect(jsonPath("$.nin").value(equalTo("12345678901")));
-    mockMvc.perform(get("/api/contacts/organisations").header("x-nin", "12345678901")).andExpect(jsonPath("$[0].orgNumber").value(equalTo("123456789")));
-    mockMvc.perform(delete("/api/organisations/testing/contacts/legal/12345678901")).andExpect(status().is(204));
-    mockMvc.perform(get("/api/organisations/testing/contacts/technical")).andExpect(status().is(200));
-    mockMvc.perform(put("/api/organisations/testing/contacts/technical/23456789012")).andExpect(status().is(204));
-    mockMvc.perform(delete("/api/organisations/testing/contacts/technical/23456789012")).andExpect(status().is(204));
-  }
+        mockMvc.perform(get("/api/assets/{org}/", org)).andExpect(status().isOk());
+        mockMvc.perform(post("/api/assets/{org}/", org).content("{ \"assetId\": \"test\", \"description\": \"Test Norge AS\" }").contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(201));
+        mockMvc.perform(get("/api/assets/{org}/{asset}", org, asset)).andExpect(status().isOk()).andExpect(jsonPath("$.name").value(equalTo(asset)));
+        mockMvc.perform(put("/api/assets/{org}/{asset}", org, asset).content("{ \"assetId\": \"test.no\", \"name\": \""+asset+"\", \"description\": \"Test Mer Norge AS\" }").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(jsonPath("$.description").value(equalTo("Test Mer Norge AS")));
+        mockMvc.perform(get("/api/adapters/{org}", org)).andExpect(status().isOk());
+        mockMvc.perform(post("/api/adapters/{org}", org).content("{ \"name\": \"testadapter\", \"note\": \"Test Adapter\", \"secret\": \"Open Sesame!\", \"shortDescription\": \"This is a Test Adapter\" }").contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(201));
+        mockMvc.perform(get("/api/adapters/{org}/{adapter}", org, adapter)).andExpect(status().isOk()).andExpect(jsonPath("$.name").value(equalTo(adapter)));
+        mockMvc.perform(get("/api/adapters/{org}/{adapter}/secret", org, adapter)).andExpect(status().isOk()).andExpect(content().string(containsString("_ClientSecret")));
+        mockMvc.perform(put("/api/adapters/{org}/{adapter}/password", org, adapter).content("This is the new password").contentType(MediaType.TEXT_PLAIN)).andExpect(status().isOk());
+        mockMvc.perform(put("/api/adapters/{org}/{adapter}", org, adapter).content("{ \"name\": \""+adapter+"\", \"note\": \"Test Adapter With New Note\", \"shortDescription\": \"This is a Brand Spanking New Test Adapter\" }").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(jsonPath("$.shortDescription").value(containsString("Spanking")));
+        mockMvc.perform(put("/api/assets/{org}/{asset}/adapters/{adapter}", org, asset, adapter)).andExpect(status().is(204));
+        mockMvc.perform(put("/api/components/{component}/{org}/adapters/{adapter}", component, org, adapter).contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(204));
+        mockMvc.perform(delete("/api/components/{component}/{org}/adapters/{adapter}", component, org, adapter)).andExpect(status().is(204));
+        mockMvc.perform(delete("/api/components/{component}/{org}/adapters/{adapter}", component, org, adapter)).andExpect(status().is(204));
+        mockMvc.perform(delete("/api/adapters/{org}/{adapter}", org, adapter)).andExpect(status().is(204));
+        mockMvc.perform(get("/api/clients/{org}", org)).andExpect(status().is(200));
+        mockMvc.perform(post("/api/clients/{org}", org).content("{ \"name\": \"testclient\", \"note\": \"Test Client\", \"secret\": \"password\", \"shortDescription\": \"This is a Test Client.\" }").contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(201)).andExpect(jsonPath("$.name").value(equalTo(client)));
+        mockMvc.perform(get("/api/clients/{org}/{client}", org, client)).andExpect(status().isOk()).andExpect(jsonPath("$.name").value(equalTo(client)));
+        mockMvc.perform(get("/api/clients/{org}/{client}/secret", org, client)).andExpect(status().isOk()).andExpect(content().string(containsString("_ClientSecret")));
+        mockMvc.perform(put("/api/clients/{org}/{client}", org, client).content("{ \"name\": \""+client+"\", \"note\": \"Testing Client\", \"shortDescription\": \"This is an updated Test Client.\" }").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(jsonPath("$.shortDescription").value(containsString("updated")));
+        mockMvc.perform(put("/api/clients/{org}/{client}/password", org, client).content("This is the new password").contentType(MediaType.TEXT_PLAIN)).andExpect(status().isOk());
+        mockMvc.perform(put("/api/assets/{org}/{asset}/clients/{client}", org, asset, client)).andExpect(status().is(204));
+        mockMvc.perform(put("/api/components/{component}/{org}/clients/{client}", component, org, client).contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(204));
+        mockMvc.perform(delete("/api/assets/{org}/{asset}/clients/{client}", org, asset, client)).andExpect(status().is(204));
+        mockMvc.perform(delete("/api/components/{component}/{org}/clients/{client}", component, org, client)).andExpect(status().is(204));
+        mockMvc.perform(delete("/api/clients/{org}/{client}", org, client)).andExpect(status().is(204));
+        mockMvc.perform(delete("/api/assets/{org}/{asset}", org, asset)).andExpect(status().is(204));
+    }
+
+    @Test
+    public void contacts() throws Exception {
+        mockMvc.perform(get("/api/contacts")).andExpect(status().is(200));
+        mockMvc.perform(get("/api/contacts/{contact}", contact1)).andExpect(status().is(200)).andExpect(jsonPath("$.nin").value(containsString(contact1)));
+        mockMvc.perform(put("/api/contacts/{contact}", contact1).content("{ \"nin\": \"12345678901\", \"firstName\": \"Tore Martin\", \"lastName\": \"Testesen\", \"mail\": \"test123@example.com\", \"mobile\": \"98765431\" }").contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(200)).andExpect(jsonPath("$.mail").value(equalTo("test123@example.com")));
+        mockMvc.perform(post("/api/contacts").content("{ \"firstName\": \"John\", \"lastName\": \"Doe\", \"mail\": \"john.doe@example.com\", \"mobile\": \"1-800-555-1212\", \"nin\": \"00000000000\" }").contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(201)).andExpect(jsonPath("$.nin").value(equalTo("00000000000")));
+        mockMvc.perform(delete("/api/contacts/00000000000")).andExpect(status().is(204));
+    }
+
+    @Test
+    public void organisation() throws Exception {
+        mockMvc.perform(get("/api/organisations/{org}/", org)).andExpect(status().is(200));
+        mockMvc.perform(put("/api/organisations/{org}/", org).content("{ \"displayName\": \"Testing Unlimited\" }").contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(200)).andExpect(jsonPath("$.displayName").value(containsString("Unlimited")));
+        mockMvc.perform(put("/api/organisations/{org}/components/{component}", org, component)).andExpect(status().is(204));
+        mockMvc.perform(delete("/api/organisations/{org}/components/{component}", org, component)).andExpect(status().is(204));
+        mockMvc.perform(put("/api/organisations/{org}/contacts/legal/{contact}", org, contact1)).andExpect(status().is(204));
+        mockMvc.perform(get("/api/organisations/{org}/contacts/legal", org)).andExpect(status().is(200)).andExpect(jsonPath("$.nin").value(equalTo(contact1)));
+        mockMvc.perform(get("/api/contacts/organisations").header("x-nin", contact1)).andExpect(jsonPath("$[0].orgNumber").value(equalTo("123456789")));
+        mockMvc.perform(delete("/api/organisations/{org}/contacts/legal/{contact}", org, contact1)).andExpect(status().is(204));
+        mockMvc.perform(get("/api/organisations/{org}/contacts/technical", org)).andExpect(status().is(200));
+        mockMvc.perform(put("/api/organisations/{org}/contacts/technical/{contact}", org, contact2)).andExpect(status().is(204));
+        mockMvc.perform(delete("/api/organisations/{org}/contacts/technical/{contact}", org, contact2)).andExpect(status().is(204));
+    }
 }
--- src/test/java/no/fint/portal/customer/test/PostmanTest.java
+++ src/test/java/no/fint/portal/customer/test/PostmanTest.java
@@ -15,15 +15,15 @@ import static org.junit.Assert.assertThat;
 @RunWith(SpringRunner.class)
 @SpringBootTest(properties = "spring.profiles.active=test", webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class PostmanTest {
-  @Ignore
-  @Test
-  public void integrationTest() throws Exception {
-    Process process = new ProcessBuilder("docker", "run", "--net=\"host\"", "-i",
-      "--add-host", "targethost:" + Inet4Address.getLocalHost().getHostAddress(),
-      "-v", Paths.get(".").toAbsolutePath().toString()+":/etc/newman",
-      "postman/newman_ubuntu1404", "run", "fint-kunde-portal.postman_collection.json", "--reporters=\"junit,cli\"",
-      "--reporter-junit-export=\"newman-report.xml\"").inheritIO().start();
-    int result = process.waitFor();
-    assertThat(result, is(0));
-  }
+    @Ignore
+    @Test
+    public void integrationTest() throws Exception {
+        Process process = new ProcessBuilder("docker", "run", "--net=\"host\"", "-i",
+                "--add-host", "targethost:" + Inet4Address.getLocalHost().getHostAddress(),
+                "-v", Paths.get(".").toAbsolutePath().toString() + ":/etc/newman",
+                "postman/newman_ubuntu1404", "run", "fint-kunde-portal.postman_collection.json", "--reporters=\"junit,cli\"",
+                "--reporter-junit-export=\"newman-report.xml\"").inheritIO().start();
+        int result = process.waitFor();
+        assertThat(result, is(0));
+    }
 }
