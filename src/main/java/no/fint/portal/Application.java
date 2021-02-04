b34f150d236e56338cc69ac7c6dc74f6a63d2403
Frode Sjovatsen <frode@fintlabs.no>
2021-02-04 15:56:30 +0100

    FKP-17 #comment Wip #in-progress

--- src/main/java/no/fint/portal/Application.java
+++ src/main/java/no/fint/portal/Application.java
@@ -6,18 +6,12 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.retry.annotation.EnableRetry;
 
-import java.util.Properties;
-
 @SpringBootApplication
 @EnableRetry
 @EnableCaching
 public class Application {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class);
-
-        Properties properties = new Properties();
-        properties.put("spring.mvc.throw-exception-if-no-handler-found", true);
-        app.setDefaultProperties(properties);
 
         app.run(args);
     }
--- src/main/java/no/fint/portal/ApplicationSecurity.java
+++ src/main/java/no/fint/portal/ApplicationSecurity.java
@@ -8,23 +8,35 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.vote.UnanimousBased;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetailsByNameServiceWrapper;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider;
 import org.springframework.security.web.authentication.preauth.RequestHeaderAuthenticationFilter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 import java.util.Collections;
 
 @Configuration
+@EnableWebFluxSecurity
 @Slf4j
-public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+public class ApplicationSecurity {
+
 
     @Autowired
     SecureUrlAccessDecisionVoter voter;
 
     @Autowired
     UserService userService;
+
+    @Bean
+    public ReactiveUserDetailsService userDetailsService() {
+        return username -> Mono.justOrEmpty(userService.loadUserByUsername(username))
+    }
+
 
     @Bean
     RequestHeaderAuthenticationFilter requestHeaderAuthenticationFilter() throws Exception {
@@ -46,16 +58,17 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
         return new UnanimousBased(Collections.singletonList(voter));
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .sessionManagement().disable()
-                .addFilter(requestHeaderAuthenticationFilter())
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.csrf().disable();
+
+        http.addFilterBefore(requestHeaderAuthenticationFilter(), SecurityWebFiltersOrder.AUTHENTICATION)
                 .authenticationProvider(preAuthenticatedAuthenticationProvider())
                 .authorizeRequests()
                 .anyRequest()
                 .fullyAuthenticated()
                 .accessDecisionManager(accessDecisionManager());
+
+        return http.build();
     }
 }
