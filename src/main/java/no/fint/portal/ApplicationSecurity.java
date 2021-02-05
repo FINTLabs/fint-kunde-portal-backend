dd7f9e5b1095a72daabf10cc343b4a7b43bedcd8
Asgeir Nilsen <asgeir@fintlabs.no>
2021-02-05 16:16:19 +0100

    Switched to WebFlux security.
    Doesn't work.
    
    FKP-17 #comment This does not work yet.

--- src/main/java/no/fint/portal/ApplicationSecurity.java
+++ src/main/java/no/fint/portal/ApplicationSecurity.java
@@ -2,29 +2,32 @@ package no.fint.portal;
 
 import lombok.extern.slf4j.Slf4j;
 import no.fint.portal.security.SecureUrlAccessDecisionVoter;
+import no.fint.portal.security.SecureUrlAuthorizationManager;
 import no.fint.portal.security.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.AccessDecisionManager;
-import org.springframework.security.access.vote.UnanimousBased;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.UserDetailsByNameServiceWrapper;
-import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider;
-import org.springframework.security.web.authentication.preauth.RequestHeaderAuthenticationFilter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.ReactivePreAuthenticatedAuthenticationManager;
+import org.springframework.security.web.server.context.ServerSecurityContextRepository;
 import reactor.core.publisher.Mono;
 
-import java.util.Collections;
+import java.util.Arrays;
 
 @Configuration
+@EnableWebSecurity
 @EnableWebFluxSecurity
 @Slf4j
 public class ApplicationSecurity {
 
+    @Value("${fint.portal.secure.paths:/api/adapters/,/api/assets/,/api/clients/,/api/components/organisation/,/api/organisations/}")
+    private String[] securePaths;
 
     @Autowired
     SecureUrlAccessDecisionVoter voter;
@@ -32,43 +35,42 @@ public class ApplicationSecurity {
     @Autowired
     UserService userService;
 
+    @Autowired
+    private ReactiveAuthenticationManager authenticationManager;
+
+    @Autowired
+    private ServerSecurityContextRepository securityContextRepository;
+
+    @Autowired
+    private SecureUrlAuthorizationManager authorizationManager;
+
     @Bean
     public ReactiveUserDetailsService userDetailsService() {
-        return username -> Mono.justOrEmpty(userService.loadUserByUsername(username))
-    }
-
-
-    @Bean
-    RequestHeaderAuthenticationFilter requestHeaderAuthenticationFilter() throws Exception {
-        RequestHeaderAuthenticationFilter requestHeaderAuthenticationFilter = new RequestHeaderAuthenticationFilter();
-        requestHeaderAuthenticationFilter.setPrincipalRequestHeader("x-nin");
-        requestHeaderAuthenticationFilter.setAuthenticationManager(authenticationManager());
-        return requestHeaderAuthenticationFilter;
+        return username -> Mono.justOrEmpty(username).map(userService::loadUserByUsername);
     }
 
     @Bean
-    PreAuthenticatedAuthenticationProvider preAuthenticatedAuthenticationProvider() {
-        PreAuthenticatedAuthenticationProvider preAuthenticatedAuthenticationProvider = new PreAuthenticatedAuthenticationProvider();
-        preAuthenticatedAuthenticationProvider.setPreAuthenticatedUserDetailsService(new UserDetailsByNameServiceWrapper<>(userService));
-        return preAuthenticatedAuthenticationProvider;
-    }
-
-    @Bean
-    public AccessDecisionManager accessDecisionManager() {
-        return new UnanimousBased(Collections.singletonList(voter));
+    public ReactiveAuthenticationManager authenticationManager(ReactiveUserDetailsService userDetailsService) {
+        return new ReactivePreAuthenticatedAuthenticationManager(userDetailsService);
     }
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        final String[] secureAntPaths = Arrays.stream(securePaths).map(p -> p + "**").toArray(String[]::new);
+        // Disable default security.
+        http.httpBasic().disable();
+        http.formLogin().disable();
         http.csrf().disable();
+        http.logout().disable();
 
-        http.addFilterBefore(requestHeaderAuthenticationFilter(), SecurityWebFiltersOrder.AUTHENTICATION)
-                .authenticationProvider(preAuthenticatedAuthenticationProvider())
-                .authorizeRequests()
-                .anyRequest()
-                .fullyAuthenticated()
-                .accessDecisionManager(accessDecisionManager());
+        // Add custom security.
+        http.authenticationManager(this.authenticationManager);
+        http.securityContextRepository(this.securityContextRepository);
+
+        http.authorizeExchange().pathMatchers(secureAntPaths).access(authorizationManager);
+        http.authorizeExchange().anyExchange().permitAll();
 
         return http.build();
     }
+
 }
