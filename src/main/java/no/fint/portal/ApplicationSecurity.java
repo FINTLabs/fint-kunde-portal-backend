14cd89278a4009b48e0af7677700950adbfdd27a
Hknots <hknots99@gmail.com>
2024-05-24 15:30:20 +0200

    convert away from deprecated API

--- src/main/java/no/fint/portal/ApplicationSecurity.java
+++ src/main/java/no/fint/portal/ApplicationSecurity.java
@@ -1,5 +1,6 @@
 package no.fint.portal;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.fint.portal.security.SecureUrlAccessDecisionVoter;
 import no.fint.portal.security.UserService;
@@ -8,37 +9,36 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.vote.UnanimousBased;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsByNameServiceWrapper;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider;
 import org.springframework.security.web.authentication.preauth.RequestHeaderAuthenticationFilter;
 
 import java.util.Collections;
 
-@Configuration
 @Slf4j
-public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationSecurity {
 
-    @Autowired
-    SecureUrlAccessDecisionVoter voter;
-
-    @Autowired
-    UserService userService;
+    private final SecureUrlAccessDecisionVoter voter;
+    private final UserService userService;
 
     @Bean
-    RequestHeaderAuthenticationFilter requestHeaderAuthenticationFilter() throws Exception {
+    public RequestHeaderAuthenticationFilter requestHeaderAuthenticationFilter(PreAuthenticatedAuthenticationProvider preAuthenticatedAuthenticationProvider) {
         RequestHeaderAuthenticationFilter requestHeaderAuthenticationFilter = new RequestHeaderAuthenticationFilter();
         requestHeaderAuthenticationFilter.setPrincipalRequestHeader("x-nin");
-        requestHeaderAuthenticationFilter.setAuthenticationManager(authenticationManager());
+        requestHeaderAuthenticationFilter.setAuthenticationManager(new ProviderManager(Collections.singletonList(preAuthenticatedAuthenticationProvider)));
         return requestHeaderAuthenticationFilter;
     }
 
     @Bean
-    PreAuthenticatedAuthenticationProvider preAuthenticatedAuthenticationProvider() {
-        PreAuthenticatedAuthenticationProvider preAuthenticatedAuthenticationProvider = new PreAuthenticatedAuthenticationProvider();
-        preAuthenticatedAuthenticationProvider.setPreAuthenticatedUserDetailsService(new UserDetailsByNameServiceWrapper<>(userService));
-        return preAuthenticatedAuthenticationProvider;
+    public PreAuthenticatedAuthenticationProvider preAuthenticatedAuthenticationProvider() {
+        PreAuthenticatedAuthenticationProvider provider = new PreAuthenticatedAuthenticationProvider();
+        provider.setPreAuthenticatedUserDetailsService(new UserDetailsByNameServiceWrapper<>(userService));
+        return provider;
     }
 
     @Bean
@@ -46,16 +46,17 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
         return new UnanimousBased(Collections.singletonList(voter));
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .sessionManagement().disable()
-                .addFilter(requestHeaderAuthenticationFilter())
+                .addFilter(requestHeaderAuthenticationFilter(preAuthenticatedAuthenticationProvider()))
                 .authenticationProvider(preAuthenticatedAuthenticationProvider())
                 .authorizeRequests()
                 .anyRequest()
                 .fullyAuthenticated()
                 .accessDecisionManager(accessDecisionManager());
+        return http.build();
     }
 }
