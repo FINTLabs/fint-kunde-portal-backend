30841bbc40423650f030ba7982c54afee99a83c4
Hknots <hknots99@gmail.com>
2024-07-05 14:29:30 +0200

    cors is now enabled

--- src/main/java/no/fint/portal/ApplicationSecurity.java
+++ src/main/java/no/fint/portal/ApplicationSecurity.java
@@ -51,6 +51,7 @@ public class ApplicationSecurity {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors().and()
                 .sessionManagement(AbstractHttpConfigurer::disable)
                 .addFilter(requestHeaderAuthenticationFilter(preAuthenticatedAuthenticationProvider()))
                 .authenticationProvider(preAuthenticatedAuthenticationProvider())
