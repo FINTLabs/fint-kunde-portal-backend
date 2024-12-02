399eadc2cb58812d8373d8acddcbaff8afcd90b7
Trond Magnus Sevre <trond@fintlabs.no>
2024-12-02 14:55:24 +0100

    Remove custom ErrorPage

--- src/main/java/no/fint/Application.java
+++ src/main/java/no/fint/Application.java
@@ -1,32 +1,15 @@
 package no.fint;
 
-import no.fint.portal.CustomContainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.retry.annotation.EnableRetry;
-
-import java.util.Properties;
 
 @SpringBootApplication
 @EnableRetry
 @EnableCaching
 public class Application {
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(Application.class);
-
-        Properties properties = new Properties();
-        properties.put("spring.mvc.throw-exception-if-no-handler-found", true);
-        app.setDefaultProperties(properties);
-
-        app.run(args);
+        SpringApplication.run(Application.class, args);
     }
-
-
-    @Bean
-    public CustomContainer containerCustomizer() {
-        return new CustomContainer();
-    }
-
 }
